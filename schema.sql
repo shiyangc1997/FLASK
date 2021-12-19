775017bc3dfe5a917e0998d027113def361b77ea
ubuntu <shiyangc1997@outlook.com>
2021-12-18 16:48:16 -0800

    second commit

---
+++ schema.sql
@@ -0,0 +1,33 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS comment;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  alias TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  like_post INTEGER NULL DEFAULT 0,
+  unlike_post INTEGER NULL DEFAULT 0,  
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE comment(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  post_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  body TEXT NOT NULL
+);
